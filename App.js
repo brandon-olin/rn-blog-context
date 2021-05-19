9dc03692abe88cb4dc5b0b31052bf74906d86e8d
“Brandon-olin <olin.brandon@gmail.com>
2021-05-19 09:27:54 -0600

    Basic CRUD actions added to application, saved in global state

--- App.js
+++ App.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import IndexScreen from './src/screens/IndexScreen';
-import { Provider } from './src/context/BlogContext';
+import React from "react";
+import { TouchableOpacity } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Provider } from "./src/context/BlogContext";
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import IndexScreen from "./src/screens/IndexScreen";
+import ShowScreen from "./src/screens/ShowScreen";
+import CreateScreen from "./src/screens/CreateScreen";
+import EditScreen from "./src/screens/EditScreen";
 
-const navigator = createStackNavigator({
-  Index: IndexScreen
-}, {
-  initialRouteName: 'Index',
-  defaultNavigationOptions: {
-    title: 'Blogs'
-  }
-})
-
-const App = createAppContainer(navigator);
-
-export default () => {
+ 
+const Stack = createStackNavigator();
+ 
+const App = () => {
   return (
-    <Provider>
-      <App />
-    </Provider>
-  )
-}
+    <NavigationContainer>
+      <Provider>
+        <Stack.Navigator>
+        <Stack.Screen
+          name="Index"
+          component={IndexScreen}
+          options={({ navigation }) => ({
+            headerTitle: 'Blogs',
+            headerRight: () => (
+              <TouchableOpacity onPress={() => navigation.navigate('Create')}>
+                <Ionicons name="add-outline" size={30} color="black" />
+              </TouchableOpacity>
+            ),
+          })}
+        />
+        <Stack.Screen
+          name="Show"
+          component={ShowScreen}
+          options={({ navigation, route }) => ({
+            headerTitle: 'Blogs',
+            headerRight: () => (
+              <TouchableOpacity onPress={() => navigation.navigate('Edit', {
+                id: route.params.id
+              })}>
+                <FontAwesome name="pencil" size={30} color="black" style={{ marginRight: 10 }} />
+              </TouchableOpacity>
+            ),
+          })}  
+        />
+        <Stack.Screen name="Create" component={CreateScreen} />
+        <Stack.Screen name="Edit" component={EditScreen} />
+        </Stack.Navigator>
+      </Provider>
+    </NavigationContainer>
+  );
+};
+ 
+export default App;
--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,20 +3,63 @@ import createDataContext from './createDataContext';
 const blogReducer = (state, action) => {
   switch (action.type) {
     case'add_blog_post':
-      return [...state, { title: `Blog Post ${state.length + 1}` }]
+      return [
+        ...state,
+        {
+          id: Math.floor(Math.random() * 99999),
+          title: action.payload.title,
+          content: action.payload.content
+        }
+      ];
+    case 'edit_blog_post':
+      return state.map(blogPost => {
+        return blogPost.id === action.payload.id ? action.payload : blogPost
+      })
+    case 'delete_blog_post':
+      return state.filter(blogPost => blogPost.id !== action.payload)
     default:
       return state
   }
 }
 
-const addBlogPost = (dispatch) => {
-  return () => {
-    dispatch({ type: 'add_blog_post' })
+const addBlogPost = dispatch => {
+  return (title, content, callback) => {
+    dispatch({
+      type: 'add_blog_post',
+      payload: { title, content }
+    });
+    callback();
+  }
+}
+
+const editBlogPost = dispatch => {
+  return (id, title, content, callback) => {
+    dispatch({
+      type: 'edit_blog_post',
+      payload: { id, title, content }
+    });
+    callback();
+  }
+}
+
+const deleteBlogPost = (dispatch) => {
+  return (id) => {
+    dispatch({ type: 'delete_blog_post', payload: id })
   }
 }
 
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost },
-  []
+  {
+    addBlogPost,
+    editBlogPost,
+    deleteBlogPost
+  },
+  [
+    {
+      title: 'Test Post',
+      content: 'Test Content',
+      id: 1
+    }
+  ]
 );
