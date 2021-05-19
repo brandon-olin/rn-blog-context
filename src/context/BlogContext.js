a128ec2d292943342229654650452ce1200412d1
“Brandon-olin <olin.brandon@gmail.com>
2021-05-19 10:06:43 -0600

    External api setup with json-server and ngrok, added initial fetch function in BlogContext to get all blog posts. Setup seed data to verify that it works correctly

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,7 +1,10 @@
 import createDataContext from './createDataContext';
+import jsonServer from '../api/jsonServer';
 
 const blogReducer = (state, action) => {
   switch (action.type) {
+    case 'get_blog_posts':
+      return action.payload
     case'add_blog_post':
       return [
         ...state,
@@ -19,6 +22,13 @@ const blogReducer = (state, action) => {
       return state.filter(blogPost => blogPost.id !== action.payload)
     default:
       return state
+  }
+}
+
+const getBlogPosts = dispatch => {
+  return async () => {
+    const response = await jsonServer.get('/blogposts');
+    dispatch({ type: 'get_blog_posts', payload: response.data })
   }
 }
 
@@ -51,15 +61,10 @@ const deleteBlogPost = (dispatch) => {
 export const { Context, Provider } = createDataContext(
   blogReducer,
   {
+    getBlogPosts,
     addBlogPost,
     editBlogPost,
     deleteBlogPost
   },
-  [
-    {
-      title: 'Test Post',
-      content: 'Test Content',
-      id: 1
-    }
-  ]
+  []
 );
